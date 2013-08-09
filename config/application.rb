753161dc5369f68cf1dc9cd6e07b590f770c44bb
Amos Stone <amos@amosarts.com>
2013-08-09 12:21:45 -0400

    Removes assets.precompile_on_init for heroku.

--- config/application.rb
+++ config/application.rb
@@ -20,5 +20,6 @@ module ModusOperandi
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
     config.assets.precompile = false
+    config.assets.initialize_on_precompile = false
   end
 end
