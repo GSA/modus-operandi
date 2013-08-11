4290ee605bbd98467566799c789b5e0e6f008431
Amos Stone <amos@amosarts.com>
2013-08-11 14:47:59 -0400

    Updates staging env for mysql db.

--- app/controllers/api/v1/registrations_controller.rb
+++ app/controllers/api/v1/registrations_controller.rb
@@ -17,4 +17,35 @@ class Api::V1::RegistrationsController < ApplicationController
 	def create
 		Registration.create(:name => params[:name])
 	end
+
+	def full_map
+		# @registration = Registration.where('ID = ?',params[:id]).joins(:sections)
+		@registration = Registration.includes(:sections).includes(:links).includes(:tags).where(:id => params[:id])
+		@json = @registration.first.attributes
+		@json[:sections] = []
+		@json[:tags] = []
+		@json[:links] = []
+		@json[:tags] << @registration.first.tag_list unless @registration.first.tag_list.empty?
+		@json[:links] = @registration.first.links unless @registration.first.links.empty?
+		@registration.first.sections.each do |section|
+			sec_hash = section.attributes
+			sec_hash[:tags] = []
+			sec_hash[:links] = []
+			# sec_hash[:tasks] = section.tasks unless section.tasks.empty?
+			sec_hash[:tasks] = []
+			sec_hash[:tags] << section.tags.attributes unless section.tags.empty?
+			sec_hash[:links] << section.links.attributes unless section.links.empty?
+			section.tasks.each do |task|
+				# sec_hash[:tasks] << task.attributes
+				task_hash = task.attributes
+				task_hash[:links] = []
+				task_hash[:tags] = []
+				task_hash[:links] = task.links unless task.links.empty?
+				task_hash[:tags] = task.tags unless task.tags.empty?
+				sec_hash[:tasks] << task_hash
+			end
+			@json[:sections] << sec_hash
+		end
+		render :json => @json
+	end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,9 @@ ModusOperandi::Application.routes.draw do
   namespace :api, :defaults => {:format => :json} do
     scope :module => :v1, :constraints => ApiConstraints.new(:version => 1, :default => true) do
       resources :registrations do
+        member do
+          get 'full_map'
+        end
         resources :sections do
           resources :tasks
         end
